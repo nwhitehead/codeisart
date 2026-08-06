6c7bb66391928564dd82381b1484aede68243edc
Nathan Whitehead <nwhitehe@gmail.com>
2026-08-05 17:49:41 -0700

    getting blurry

--- art3/shaders/conv.glsl
+++ art3/shaders/conv.glsl
@@ -1,3 +1,5 @@
+const int W = 5;
+const int H = 5;
 
 vec3 fbm3(vec3 x, int iters, float fratio, float wratio) {
    vec3 y = vec3(0.0);
@@ -17,9 +19,9 @@ vec3 fbm3(vec3 x, int iters, float fratio, float wratio) {
 void main()
 {
     vec4 color = vec4(0.0, 0.0, 0.0, 1.0);
-    vec2 st = gl_FragCoord.xy / u_resolution.xy;
     vec3 p = gl_FragCoord.xyz / u_resolution.x;
-    vec4 col = texture2D(u_tex0, p.xy);
+    float csz = 30.0 / u_resolution.x;
+    vec4 col = texture2D(u_tex0, p.xy) + texture2D(u_tex0, p.xy + vec2(csz, csz));
     p.z = u_time * 0.03;
     //vec3 p2 = fbm3(fbm3(p * 2.0, 8, 2.0, 0.5) * 0.5 + p * 10.0 + vec3(2.2, 0.0, 0.0) * u_time * 0.0, 8, 2.0, 0.5);
     vec3 p2 = fbm3(0.1 * fbm3(0.5 * fbm3(p * 2.0, 8, 2.0, 0.5) * 0.5 + p * 10.0, 8, 2.0, 0.5), 8, 2.0, 0.5);
