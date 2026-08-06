0e09b8c9d5281b19ab4cd168dbe63e6bcbc6a454
Nathan Whitehead <nwhitehe@gmail.com>
2026-08-05 22:08:27 -0700

    inky stars

--- art3/shaders/conv.glsl
+++ art3/shaders/conv.glsl
@@ -19,9 +19,11 @@ vec3 fbm3(vec3 x, int iters, float fratio, float wratio) {
 void main()
 {
     vec3 p = gl_FragCoord.xyz / u_resolution.x;
+    p.z = u_time * 0.005;
     float csz = 8.0 / u_resolution.x;
     vec4 col = vec4(0.0);
     vec2 toff = fbm3(vec3(p.xy, u_time), 8, 2.0, 0.5).xy * 0.01;
-    col += texture2D(u_tex0, p.xy + toff);
+    vec3 p2 = fbm3(0.1 * fbm3(0.5 * fbm3(p * 2.0, 8, 2.0, 0.5) * 0.5 + p * 10.0, 8, 2.0, 0.5), 8, 2.0, 0.5);
+    col += texture2D(u_tex0, p.xy + p2.xy * 0.05);
     gl_FragColor = col;
 }
