7c3fb7b8a6fa6d4358ba7ddfdee877fdb67f3b65
Nathan Whitehead <nwhitehe@gmail.com>
2026-08-05 22:04:03 -0700

    watery

--- art3/shaders/conv.glsl
+++ art3/shaders/conv.glsl
@@ -18,27 +18,10 @@ vec3 fbm3(vec3 x, int iters, float fratio, float wratio) {
 
 void main()
 {
-    vec4 color = vec4(0.0, 0.0, 0.0, 1.0);
     vec3 p = gl_FragCoord.xyz / u_resolution.x;
     float csz = 8.0 / u_resolution.x;
     vec4 col = vec4(0.0);
-    float a = 2.0 + (cos(u_time * 2.0) * 0.5 + 0.5) * 20.0;
-    float norma = sqrt(a / PI / float(C));
-    for (int i = -C/2; i <= C/2; i++) {
-        for (int j = -C/2; j <= C/2; j++) {
-            vec2 uvo = vec2(float(i), float(j)) / float(C/2);
-            float d = length(uvo);
-            //float coeff = norma * norma * exp(-a * d*d);
-            //float coeff = fbm3(vec3(uvo * 1000.01, u_time), 8, 2.0, 0.5).y;
-            float coeff = fbm3(vec3(uvo.xy * 100.0, u_time), 8, 2.0, 0.5).y;
-            col += coeff * texture2D(u_tex0, p.xy + uvo * csz);
-        }
-    }
-    p.z = u_time * 0.03;
-    //vec3 p2 = fbm3(fbm3(p * 2.0, 8, 2.0, 0.5) * 0.5 + p * 10.0 + vec3(2.2, 0.0, 0.0) * u_time * 0.0, 8, 2.0, 0.5);
-    vec3 p2 = fbm3(0.1 * fbm3(0.5 * fbm3(p * 2.0, 8, 2.0, 0.5) * 0.5 + p * 10.0, 8, 2.0, 0.5), 8, 2.0, 0.5);
-    float n = p2.y;
-    //color = vec4(1.0, 1.0, 1.0, 1.0) * p.x;
-    color = col;
-    gl_FragColor = color;
+    vec2 toff = fbm3(vec3(p.xy, u_time), 8, 2.0, 0.5).xy * 0.01;
+    col += texture2D(u_tex0, p.xy + toff);
+    gl_FragColor = col;
 }
