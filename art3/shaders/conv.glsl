e320a7045234c3295945823f853177d519d9a4e7
Nathan Whitehead <nwhitehe@gmail.com>
2026-08-05 19:53:57 -0700

    conv blur

--- art3/shaders/conv.glsl
+++ art3/shaders/conv.glsl
@@ -1,5 +1,5 @@
-const int W = 5;
-const int H = 5;
+const int C = 7;
+const float PI = 3.14159265;
 
 vec3 fbm3(vec3 x, int iters, float fratio, float wratio) {
    vec3 y = vec3(0.0);
@@ -20,8 +20,18 @@ void main()
 {
     vec4 color = vec4(0.0, 0.0, 0.0, 1.0);
     vec3 p = gl_FragCoord.xyz / u_resolution.x;
-    float csz = 30.0 / u_resolution.x;
-    vec4 col = texture2D(u_tex0, p.xy) + texture2D(u_tex0, p.xy + vec2(csz, csz));
+    float csz = 8.0 / u_resolution.x;
+    vec4 col = vec4(0.0);
+    float a = 2.0 + (cos(u_time * 2.0) * 0.5 + 0.5) * 20.0;
+    float norma = sqrt(a / PI / float(C));
+    for (int i = -C/2; i <= C/2; i++) {
+        for (int j = -C/2; j <= C/2; j++) {
+            vec2 uvo = vec2(float(i), float(j)) / float(C/2);
+            float d = length(uvo);
+            float coeff = norma * norma * exp(-a * d*d);
+            col += coeff * texture2D(u_tex0, p.xy + uvo * csz);
+        }
+    }
     p.z = u_time * 0.03;
     //vec3 p2 = fbm3(fbm3(p * 2.0, 8, 2.0, 0.5) * 0.5 + p * 10.0 + vec3(2.2, 0.0, 0.0) * u_time * 0.0, 8, 2.0, 0.5);
     vec3 p2 = fbm3(0.1 * fbm3(0.5 * fbm3(p * 2.0, 8, 2.0, 0.5) * 0.5 + p * 10.0, 8, 2.0, 0.5), 8, 2.0, 0.5);
